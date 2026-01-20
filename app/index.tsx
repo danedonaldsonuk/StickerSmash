3db8415ed749b0e87933900ac08ec82549326763
DaneTheMuffin <daneoh98@gmail.com>
2026-01-20 18:48:59 +0000

    Initial Expo project

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e",
+  },
+  text: {
+    color: "#ffffff",
+  },
+});
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
+    </View>
+  );
+}
